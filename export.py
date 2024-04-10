14cb00fe93a3cd8a695a0d699447693002334df6
Bradford M. Knowlton Jr <bradford.knowlton@westerncpe.com>
2024-04-10 14:17:45 -0600

    finished version

--- export.py
+++ export.py
@@ -7,11 +7,69 @@ import requests
 import json
 import time
 
+
 # Load environment variables from a .env file
 from dotenv import load_dotenv
+from crisp_api import Crisp
+from datetime import datetime
+
 
 load_dotenv()
 
+client = Crisp()
+
+identifier = os.environ["CRISP_IDENTIFIER"]
+key = os.environ["CRISP_API_KEY"]
+
+website_id = os.environ["CRISP_WEBSITE_ID"]
+
+client.set_tier("plugin")
+client.authenticate(identifier, key)
+
+# /v1/website/{website_id}
+
+# session_id = "session_1cfe5a44-ea05-4ae6-937d-37c0030019bd"
+
+# client.website.send_message_in_conversation(
+#   website_id, session_id,
+
+#   {
+#     "type": "text",
+#     "content": "This message was sent from python-crisp-api! :)",
+#     "from": "operator",
+#     "origin": "chat"
+#   }
+# )
+
+# 37f8a814-52d5-4cdf-afba-ac0cbc82fbcf
+
+# people_id = "37f8a814-52d5-4cdf-afba-ac0cbc82fbcf"
+
+# data = client.website.get_people_data(website_id, people_id)
+
+# data = client.website.get_people_statistics(website_id)
+
+# page_number = 1
+
+# data = client.website.list_people_profiles(website_id, page_number)
+
+# results = client.website.get_people_profile(website_id, people_id);
+
+# data = {
+#     "notepad": "Favorite Number: 10197\n\
+# by: John Doe - 10:41am 04/10/2024\n\
+# \n\
+# Favorite Color: Blue\n\
+# by: John Doe - 05:41pm 12/03/2023"
+# }
+
+# results = client.website.update_people_profile(website_id, people_id, data)
+
+# people_id = "christopher.moyer@westerncpe.com"
+
+# results = client.website.get_people_profile(website_id, people_id)
+
+# print(results)
 
 # Python function to cache an API call in a folder called export, using an md5 hash of the request url as the filename
 def cache_api_call(url, headers, params):
@@ -62,6 +120,8 @@ headers = {
     "authorization": "Bearer "+os.environ["FRONT_API_KEY"],
 }
 
+# url = False
+
 while url:
 
     response = cache_api_call(url, headers=headers, params=[])
@@ -70,10 +130,80 @@ while url:
     # print(response["_pagination"]["next"])
 
     for contact in response["_results"]:
+
+        # print(contact["name"])
+        # print(contact["handles"][0]["handle"])
+        # print(contact["handles"])
+        email = False
+
+        for handle in contact["handles"]:
+            if handle["source"] == "email":
+                email = handle["handle"]
+                break
+
+        print(email)
+        if not email:
+            # print("No email found for contact")
+            continue
+
+        
+
         # print(contact["_links"]["related"]["notes"])
         url = contact["_links"]["related"]["notes"]
         notes = cache_api_call(url, headers=headers, params=[])
 
+        # print(notes["_results"])
+        # [{'_links': {'related': {'author': 'https://wcpe.api.frontapp.com/teammates/tea_amqtw', 'owner': None}}, 
+        # 'author': {'_links': {'self': 'https://wcpe.api.frontapp.com/teammates/tea_amqtw', 
+        # 'related': {'inboxes': 'https://wcpe.api.frontapp.com/teammates/tea_amqtw/inboxes', 
+        # 'conversations': 'https://wcpe.api.frontapp.com/teammates/tea_amqtw/conversations'}}, 
+        # 'id': 'tea_amqtw', 'email': 'cary.miller@westerncpe.com', 'username': 'cary.miller', 
+        # 'first_name': 'Cary', 'last_name': 'Miller', 'is_admin': False, 'is_available': False, 'is_blocked': True, 
+        # 'custom_fields': {}}, 'body': 'General inquiry on certificates from Global. did not provide name.', 
+        # 'created_at': 1712013870.996, 'is_private': False}]
+
+        if len(notes["_results"]):
+
+            # email = "christopher.moyer@westerncpe.com"
+            
+            try:
+                profile = client.website.get_people_profile(website_id, email)
+            except:
+                print("No profile found for email: "+email)
+                continue
+
+            print(profile["people_id"])
+            people_id = profile["people_id"]
+            notepad = []
+            if "notepad" in profile and profile["notepad"] != "":
+                notepad.append(profile["notepad"])
+
+            print(email)
+
+            for note in notes["_results"]:
+                # print(note["body"])
+                # print(note["author"]["first_name"])
+                # print(note["author"]["last_name"])
+                # print(note["author"]["email"])
+                # print(note["created_at"])
+
+                # # convert the timestamp to a datetime object in the local timezone
+                # dt_object = datetime.fromtimestamp(note["created_at"])
+
+                # # print the datetime object and its type
+                # print(dt_object)
+                notepad.append( note["body"] + "\n" + note["author"]["first_name"] + " " + note["author"]["last_name"] + 
+                " (" + note["author"]["email"] + ")" + "\n" + str(datetime.fromtimestamp(note["created_at"]).strftime("%x %I:%M%p")) + "\n" )
+            print(notepad)
+            # people_id = "37f8a814-52d5-4cdf-afba-ac0cbc82fbcf"
+            data = {
+                "notepad": "\n".join(notepad)
+            }
+            results = client.website.update_people_profile(website_id, people_id, data)
+            print(results)
+
+
+    # url = False
     if  response["_pagination"]["next"]:
         print("There are more pages")
         url = response["_pagination"]["next"]
